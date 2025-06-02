99b3edf5331d837fbea2e5dd000dc95d9561af6b
Joe Ganser <jkgprofessional@gmail.com>
2025-06-02 11:08:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import yaml
 
 
 
-configFile = '5650.yaml'
+configFile = '25650.yaml'
 def load_config(path=f"configFiles/{configFile}"):
     with open(path, "r") as file:
         config = yaml.safe_load(file)
